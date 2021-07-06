4b3717cbdcd75c2b242f6f2dff1c042249263cad
ska7 <nikkofann@gmail.com>
2021-07-06 19:46:19 +0300

    Add version 1.0.1

--- src/App.js
+++ src/App.js
@@ -1,104 +1,76 @@
-import { TextField, Container, Button, Typography } from "@material-ui/core";
-import { makeStyles, createStyles } from "@material-ui/core/styles";
+import { Typography, Container, TextField } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import jumperIcon from "./dashboardJumperIcon128.png";
 
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
-const useStyles = makeStyles((theme) =>
-  createStyles({
-    container: {
-      height: "200px",
-      width: "300px",
-      display: "flex",
-      flexDirection: "column",
-      alignItems: "center",
-      justifyContent: "center",
-      margin: 0,
-      padding: 0,
-      border: "1px solid black",
-    },
-    input: {
-      display: "flex",
-      justifyContent: "center",
-      height: "30%",
-      width: "80%",
-    },
-    inputProps: {
+const useStyles = makeStyles({
+  container: {
+    height: "200px",
+    width: "300px",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    margin: 0,
+    padding: 0,
+    border: "1px solid black",
+  },
+  input: {
+    display: "flex",
+    justifyContent: "center",
+    height: "30%",
+    width: "80%",
+  },
+  inputProps: {
+    fontSize: "1em",
+    width: "100%",
+    "&::placeholder": {
       fontSize: "1em",
-      width: "100%",
-      "&::placeholder": {
-        fontSize: "1em",
-      },
     },
-    button: {
-      color: "#379CCA",
-      border: "1px solid #379CCA",
-      width: "80%",
-      height: "20%",
-    },
-    alert: {
-      position: "absolute",
-      color: "red",
-      fontWeight: "bold",
-      top: "20px",
-      fontSize: "0.6em",
-    },
-  })
-);
+  },
+  alert: {
+    position: "absolute",
+    color: "red",
+    fontWeight: "bold",
+    top: "20px",
+    fontSize: "0.6em",
+  },
+});
 
 function App() {
-  const [id, setId] = useState("");
-  const [showAlert, setShowAlert] = useState(false);
+  const [error, setError] = useState("");
+  const [value, setValue] = useState("");
 
-  const handleClick = () => {
-    if (id.length <= 5) {
-      setShowAlert(true);
+  const handleChange = (event) => {
+    const id = event.target.value;
+    setValue(id);
+    if (id.length >= 6 && id.length) {
+      window.open(`https://accounts.bizzabo.com/144879/events/${id}`);
+      setError("");
+      setValue("");
+    } else if (id.length === 0) {
+      setError("");
     } else {
-      setShowAlert(false);
-      window.open(`https://accounts.bizzabo.com/128368/events/${id}`);
+      setError("Invalid ID, make sure it's 6-digit long");
     }
   };
 
-  useEffect(() => {
-    // navigator.clipboard.readText().then((clipText) => {
-    //   if (clipText(typeof Number) || clipText(typeof String)) {
-    //     window.open(`https://accounts.bizzabo.com/128368/events/${clipText}`);
-    //   } else {
-    //     alert("Invalid ID, please copy the correct event ID");
-    //   }
-    // });
-  }, []);
-
   const classes = useStyles();
   return (
-    <Container className={classes.container} disableGutters maxWidth={false}>
-      {showAlert ? (
-        <Typography className={classes.alert}>
-          Invalid ID, please make sure it's 6-digit long ;)
-        </Typography>
+    <Container className={classes.container}>
+      {error ? (
+        <Typography className={classes.alert}>{error}</Typography>
       ) : null}
       <TextField
-        value={id}
-        onChange={(e) => setId(e.target.value)}
-        color="primary"
-        inputProps={{
-          className: classes.inputProps,
-          maxLength: "7",
-          minLength: "6",
-        }}
+        value={value}
+        onChange={handleChange}
         className={classes.input}
-        placeholder="Paste event ID here"
+        inputProps={{ className: classes.inputProps }}
       />
-      <Button
-        variant="outlined"
-        className={classes.button}
-        onClick={handleClick}
-      >
-        GO
-      </Button>
+      <img src={jumperIcon} alt="jumper-img" className={classes.icon} />
     </Container>
   );
 }
 
 export default App;
-
-// https://www.youtube.com/watch?v=ot-dSAIEkK8
